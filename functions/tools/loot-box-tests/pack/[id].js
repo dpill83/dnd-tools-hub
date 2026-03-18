aa34caf30f8ae6a498f48d246ecdea88ff8bb57d
dpill1 <dphillips83@gmail.com>
2026-03-17 19:05:08 -0500

    Remove obsolete redirect files from loot box tests: Delete _redirects files in the root and tools/loot-box-tests directories to streamline project structure and eliminate unused components.

---
+++ functions/tools/loot-box-tests/pack/[id].js
@@ -0,0 +1,5 @@
+export async function onRequest(context) {
+  const url = new URL(context.request.url);
+  url.pathname = '/tools/loot-box-tests/pack.html';
+  return context.env.ASSETS.fetch(new Request(url.toString(), context.request));
+}
